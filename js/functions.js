413d09b6a59de2db67f587e8f10888548a6b5d23
andre-de-kaymagne <akaymanov@yandex.ru>
2023-06-22 16:30:39 +0300

    Добавил функцию printResults

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,9 @@
+const printResults = (...args) => {
+  for (let i = 0; i < args.length / 2; i++) {
+    console.log(args[i * 2], args[i * 2 + 1]);
+  }
+};
+
 const checkTime = (beginOfDay, endOfDay, startOfMeet, duration) => {
   beginOfDay = Number(beginOfDay.replace(':', '.'));
   endOfDay = Number(endOfDay.replace(':', '.'));
@@ -8,4 +14,10 @@ const checkTime = (beginOfDay, endOfDay, startOfMeet, duration) => {
   return Boolean(workHard <= workNormal && startOfMeet >= beginOfDay);
 };
 
-checkTime('8:0', '10:0', '8:0', 120); // true
+printResults(
+  'имяФункции(\'08:00\', \'17:30\', \'14:00\', 90) = ', checkTime('08:00', '17:30', '14:00', 90),
+  'имяФункции(\'8:0\', \'10:0\', \'8:0\', 120) = ', checkTime('8:0', '10:0', '8:0', 120),
+  'имяФункции(\'08:00\', \'14:30\', \'14:00\', 90) = ', checkTime('08:00', '14:30', '14:00', 90),
+  'имяФункции(\'14:00\', \'17:30\', \'08:0\', 90) = ', checkTime('14:00', '17:30', '08:0', 90),
+  'имяФункции(\'8:00\', \'17:30\', \'08:00\', 900) = ', checkTime('8:00', '17:30', '08:00', 900),
+);
