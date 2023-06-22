6cfd1c5ba3f4d281198bf31048a10639f8af0d80
andre-de-kaymagne <akaymanov@yandex.ru>
2023-06-22 21:31:15 +0300

    Разбил на модули.

---
+++ js/data.js
@@ -0,0 +1,32 @@
+const DESCRIPTION_PHOTO = [
+  'Lorem ipsum dolor sit amet.',
+  'consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+  'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.',
+  'Sed ut perspiciatis unde omnis iste natus error sit voluptatem.',
+  'At vero eos et accusamus et iusto odio dignissimos ducimus.',
+  'Nemo enim ipsam voluptatem quia voluptas sit aspernatur.',
+  'Quis autem vel eum iure reprehenderit qui in ea voluptate velit.',
+  ' Excepteur sint occaecat cupidatat non proident, sunt in culpa.',
+];
+
+const NAMES_OF_COMMENTATORS = [
+  'Иван',
+  'Андрей',
+  'Марина',
+  'Константин',
+  'Александр',
+  'Варвара',
+  'Екатерина',
+  'Мария',
+];
+
+const MESSAGES_OF_COMMENTATORS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+export{DESCRIPTION_PHOTO, NAMES_OF_COMMENTATORS, MESSAGES_OF_COMMENTATORS};
