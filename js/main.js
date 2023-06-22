de44b2622063b68be01862c71f6e661f254550f5
andre-de-kaymagne <akaymanov@yandex.ru>
2023-06-22 21:30:46 +0300

    Разбил файл main.js на модули. Подключил в Индекс.

---
+++ js/main.js
@@ -0,0 +1,30 @@
+import{DESCRIPTION_PHOTO, NAMES_OF_COMMENTATORS, MESSAGES_OF_COMMENTATORS} from 'data.js';
+import{createUnicumId, getRandomArrayElement} from 'utile.js';
+
+const licesCount = createUnicumId(15, 200);
+const generatePhotoId = createUnicumId(1, 25);
+const generateCommentatorId = createUnicumId(1, 25);
+const generateAvatarId = createUnicumId(1, 6);
+const SIMILAR_PHOTO_COUNT = 4;
+const SIMILAR_COMMENTS_COUNT = 3;
+
+const createComment = () => ({
+  id: generateCommentatorId(),
+  avatar: `img/avatar-${ generateAvatarId() }.svg`,
+  message: getRandomArrayElement(MESSAGES_OF_COMMENTATORS),
+  name: getRandomArrayElement(NAMES_OF_COMMENTATORS),
+});
+
+const similarComment = Array.from({length:SIMILAR_COMMENTS_COUNT}, createComment);
+
+const createPhoto = () => ({
+  id: generatePhotoId(),
+  url: `photos/${ generatePhotoId() }.jpg`,
+  description: getRandomArrayElement(DESCRIPTION_PHOTO),
+  likes: licesCount(),
+  comments: similarComment,
+});
+
+const createPictures = () => Array.from({length:SIMILAR_PHOTO_COUNT}, createPhoto);
+
+createPictures();
